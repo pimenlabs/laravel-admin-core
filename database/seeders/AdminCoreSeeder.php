5a59e56cb5823889492cb051f1d34e8b0cb0f1a0
balajidharma <balajidharma@hotmail.com>
2024-05-20 15:28:54 -0400

    Added media manager related changes

--- database/seeders/AdminCoreSeeder.php
+++ database/seeders/AdminCoreSeeder.php
@@ -52,6 +52,10 @@ class AdminCoreSeeder extends Seeder
             'category.type create',
             'category.type edit',
             'category.type delete',
+            'media list',
+            'media create',
+            'media edit',
+            'media delete',
         ];
 
         foreach ($permissions as $permission) {
@@ -143,6 +147,13 @@ class AdminCoreSeeder extends Seeder
                 'enabled' => 1,
                 'weight' => 4,
                 'icon' => 'M5 3A2 2 0 0 0 3 5H5M7 3V5H9V3M11 3V5H13V3M15 3V5H17V3M19 3V5H21A2 2 0 0 0 19 3M3 7V9H5V7M7 7V11H11V7M13 7V11H17V7M19 7V9H21V7M3 11V13H5V11M19 11V13H21V11M7 13V17H11V13M13 13V17H17V13M3 15V17H5V15M19 15V17H21V15M3 19A2 2 0 0 0 5 21V19M7 19V21H9V19M11 19V21H13V19M15 19V21H17V19M19 19V21A2 2 0 0 0 21 19Z',
+            ],
+            [
+                'name' => 'Media',
+                'uri' => '/<admin>/media',
+                'enabled' => 1,
+                'weight' => 5,
+                'icon' => 'M9 13V5C9 3.9 9.9 3 11 3H20C21.1 3 22 3.9 22 5V11H18.57L17.29 9.26C17.23 9.17 17.11 9.17 17.05 9.26L15.06 12C15 12.06 14.88 12.07 14.82 12L13.39 10.25C13.33 10.18 13.22 10.18 13.16 10.25L11.05 12.91C10.97 13 11.04 13.15 11.16 13.15H17.5V15H11C9.89 15 9 14.11 9 13M6 22V21H4V22H2V2H4V3H6V2H8.39C7.54 2.74 7 3.8 7 5V13C7 15.21 8.79 17 11 17H15.7C14.67 17.83 14 19.08 14 20.5C14 21.03 14.11 21.53 14.28 22H6M4 7H6V5H4V7M4 11H6V9H4V11M4 15H6V13H4V15M6 19V17H4V19H6M23 13V15H21V20.5C21 21.88 19.88 23 18.5 23S16 21.88 16 20.5 17.12 18 18.5 18C18.86 18 19.19 18.07 19.5 18.21V13H23Z',
             ],
         ];
 
